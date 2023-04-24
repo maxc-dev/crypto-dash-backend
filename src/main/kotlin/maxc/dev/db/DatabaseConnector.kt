676b3d6cdead36abd2b3733af7052c31ea331319
maxc-dev <mer.cartermc@gmail.com>
2023-04-24 21:45:54 +0100

    Added price change manager and listener interface setup

---
+++ src/main/kotlin/maxc/dev/db/DatabaseConnector.kt
@@ -0,0 +1,12 @@
+package maxc.dev.db
+
+import org.ktorm.database.Database
+
+class DatabaseConnector(url: String, user: String, password: String) {
+    val database = Database.connect(
+        url = "jdbc:postgresql://$url",
+        user = user,
+        password = password,
+        driver = "org.postgresql.Driver"
+    )
+}
