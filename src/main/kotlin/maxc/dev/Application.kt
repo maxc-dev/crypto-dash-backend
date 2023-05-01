1eacb7b85e6b399dae2085d98fe5997bd37d018d
maxc-dev <mer.cartermc@gmail.com>
2023-05-01 14:19:38 +0100

    Merged Application.kt

--- src/main/kotlin/maxc/dev/Application.kt
+++ src/main/kotlin/maxc/dev/Application.kt
@@ -4,11 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.*
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
 import maxc.dev.db.DatabaseConnector
-import maxc.dev.engine.PriceChange
 import maxc.dev.engine.PriceChangeManager
 import maxc.dev.engine.PriceUploader
 import maxc.dev.plugins.kafka.KafkaTopicManager
@@ -19,8 +15,6 @@ import maxc.dev.provider.ticker.BinanceMarketMiniTickerAllBase
 import maxc.dev.provider.ticker.PriceTickerModel
 import org.apache.kafka.clients.admin.KafkaAdminClient
 import maxc.dev.dao.DatabaseFactory
-import java.io.File
-import kotlin.text.StringBuilder
 
 fun main() {
     embeddedServer(Netty, port = 8000, host = "0.0.0.0", module = Application::module)
